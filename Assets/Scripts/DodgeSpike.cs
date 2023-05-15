d349229f6a2c76776302a0c3e2fad4b1be9d6444
Andy <dumpyee09@gmail.com>
2023-05-15 16:58:33 +1000

    REVAMP LEVEL 3 + FONT + SPRITES

--- Assets/Scripts/DodgeSpike.cs
+++ Assets/Scripts/DodgeSpike.cs
@@ -4,15 +4,13 @@ public class DodgeSpike : MonoBehaviour
 {
     public GameObject player;
     public GameObject spike;
-    public GameObject ground;
-
+    
     public float fallSpeed = 5f;
     public float spawnHeight = 10f;
     public float spawnDelay = 1f;
     private void Start()
     {
         InvokeRepeating(nameof(SpawnSpike), 0f, spawnDelay);
-        InvokeRepeating(nameof(SpawnGround), 0f, spawnDelay);
     }
 
     void SpawnSpike()
@@ -24,18 +22,7 @@ public class DodgeSpike : MonoBehaviour
         rb.gravityScale = fallSpeed;
         s.transform.rotation = Quaternion.Euler(0, 0, 180);
     }
-
-    void SpawnGround()
-    {
-        // Spawn ground under the player
-        GameObject g=  Instantiate(ground, new Vector3(player.transform.position.x, player.transform.position.y, 0), Quaternion.identity);
-        WaitForSeconds wait = new WaitForSeconds(0.4f);
-        Rigidbody2D rb = g.AddComponent<Rigidbody2D>();
-        rb.gravityScale = fallSpeed;
-        g.transform.localScale = new Vector3(1, 0.1f, 1);
-        
-    }
-
+    
     private void Update()
     {
         if (player.transform.position.y > spawnHeight)
